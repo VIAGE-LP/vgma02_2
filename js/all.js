1db0e7f383c98fd47b2d7cc83d5af141435d1c7f
Pickachu Chen <pickachu1006@gmail.com>
2020-08-27 14:09:48 +0800

    20200827_vgma02 backup

---
+++ js/all.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    // 圖片lozad
+    const observer = lozad('.lozad', {
+        rootMargin: "150px 0px"
+    });
+    observer.observe();
+    // 立即購買btn動態
+    $('.offerbtn').click(function (e) { 
+        e.preventDefault();
+        $('html , body').animate({
+            scrollTop: $('#size').offset().top,
+        },900);
+    });
+    // 立即訂購btn動態
+    // $('.buybtn').click(function (e) { 
+    //     e.preventDefault();
+    //     $('html , body').animate({
+    //         scrollTop: $('#size').offset().top,
+    //     },900);
+    // });
+
+    //偵測sns line btn 手機版還是電腦版，連結不同
+    var vw=$(window).width();
+    if (vw <= 768) {
+        $(".line_link").attr("href", "https://bit.ly/2YszXqD");
+    } else {
+        $(".line_link").attr("href", "https://line.me/R/ti/p/%40fhl1857j");
+    }
+});
